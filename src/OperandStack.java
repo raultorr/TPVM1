1cc847d9c2ed779ab25584b73772936ec9d41236
Raúl Torrijos <raultorr@ucm.es>
2016-10-13 23:19:50 +0200

    Clase OperandStack.java añadida
    Añadido cuerpo de manera genérica siguiendo el enunciado del Assignment
    1.

--- src/OperandStack.java
+++ src/OperandStack.java
@@ -1,4 +1,14 @@
 
 public class OperandStack {
 
+	private int[] stack;
+	
+	public void setValue(int num, int pos){
+		this.stack[pos] = num;
+	}
+	
+	public int getValue(int pos) {
+		return this.stack[pos];
+	}
+	
 }
