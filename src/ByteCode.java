2224075b15295c9f28f5e1dfcbdec5ab9a988574
Raúl Torrijos <raultorr@ucm.es>
2016-10-13 22:50:19 +0200

    Clases ByteCode y ByteCodeParser editadas
    +Métodos añadidos para modificarlos en un futuro

--- src/ByteCode.java
+++ src/ByteCode.java
@@ -1,4 +1,24 @@
 
 public class ByteCode {
-
+	
+	public static enum ENUM_BYTECODE {push,load,store,add,sub,div,out,halt}
+	private ENUM_BYTECODE name;
+	private int param;																//Used with instructions: push, store and load
+	
+	public ByteCode (ENUM_BYTECODE instructionType){								//Normal constructor of the class ByteCode
+		this.name = instructionType;	
+	}
+	
+	public ByteCode (ENUM_BYTECODE instructionType, int num){						//Constructor used for the PUSH, STORE, LOAD instructions
+		this.name = instructionType;
+		this.setParam(num);
+	}
+	
+	public void setParam (int num){
+		this.param = num;
+	}
+	
+	public int getParam(){
+		return this.param;
+	}
 }
--- src/ByteCodeParser.java
+++ src/ByteCodeParser.java
@@ -1,4 +1,29 @@
 
 public class ByteCodeParser {
-
+	
+	public static ByteCode parse (String s){
+		ByteCode instruction = null;
+		String[] arrayOfStrings = s.split(" ");
+		
+		if (arrayOfStrings[0].equalsIgnoreCase("push")) {	
+			instruction = new ByteCode(ByteCode.ENUM_BYTECODE.push, Integer.parseInt(arrayOfStrings[1]));
+		} else if(arrayOfStrings[0].equalsIgnoreCase("load")) {
+			instruction = new ByteCode(ByteCode.ENUM_BYTECODE.load, Integer.parseInt(arrayOfStrings[1]));
+		} else if(arrayOfStrings[0].equalsIgnoreCase("store")) {
+			instruction = new ByteCode(ByteCode.ENUM_BYTECODE.store, Integer.parseInt(arrayOfStrings[1]));
+		} else if(arrayOfStrings[0].equalsIgnoreCase("add")) {
+			instruction = new ByteCode(ByteCode.ENUM_BYTECODE.add);
+		} else if(arrayOfStrings[0].equalsIgnoreCase("sub")) {
+			instruction = new ByteCode(ByteCode.ENUM_BYTECODE.sub);
+		} else if(arrayOfStrings[0].equalsIgnoreCase("div")) {
+			instruction = new ByteCode(ByteCode.ENUM_BYTECODE.div);
+		} else if(arrayOfStrings[0].equalsIgnoreCase("out")) {
+			instruction = new ByteCode(ByteCode.ENUM_BYTECODE.out);
+		} else if(arrayOfStrings[0].equalsIgnoreCase("halt")) {
+			instruction = new ByteCode(ByteCode.ENUM_BYTECODE.halt);
+		}
+		
+		return instruction;
+	}
+		
 }
