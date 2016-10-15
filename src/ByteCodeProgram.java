5d655dce1f2ade749bea7fecf02c4ced8c8532fe
Raúl Torrijos <raultorr@ucm.es>
2016-10-15 14:02:29 +0200

    ByteCodeProgram.java completado y pequeños cambios en Engine

--- src/ByteCodeProgram.java
+++ src/ByteCodeProgram.java
@@ -1,4 +1,41 @@
+import java.util.Arrays;
 
 public class ByteCodeProgram {
+	
+	private ByteCode[] program;
+	
+	public void setNextByteCodeInstruction(ByteCode nextInstruction){
+		extendByteCodeArrayLength(1);
+		program[program.length-1] = nextInstruction;
+	}
+	
+	public void removeLastByteCodeInstruction(){
+		reduceByteCodeArrayLength(1);
+	}
+	
+	public ByteCode getByteCodeInstructionOnPosition(int n){
+		return program[n];
+	}
+	
+	public void placeByteCodeInstruction(ByteCode newInstruction, int n){
+		program[n] = newInstruction;
+	}
+	
+	private void extendByteCodeArrayLength(int n) {
+		program = Arrays.copyOf(program,program.length + n );
+	}
+	
+	private void reduceByteCodeArrayLength(int n) {
+		program = Arrays.copyOf(program,program.length - n );
+	}
+	
+	public String toString (){
+		String result="Current program:\n";
+		for (int i=0; i < program.length; i++){
+			result = result + i + ": " + program[i].toString() + "\n";
+		}
+	
+		return result;
+	}
 
 }
--- src/Engine.java
+++ src/Engine.java
@@ -2,20 +2,25 @@ import java.util.*;
 
 public class Engine {
 
+		private ByteCodeProgram program;
+		private boolean end=false;
 	
 		public void start() {
 			System.out.println("Type a command: ");					
 			Scanner scanner = new Scanner(System.in);
-			String rawInput = scanner.nextLine();
-			Command command;
-			command = CommandParser.parse(rawInput);
-			if (command.equals(null)){
-				System.err.println("Error: incorrect command");
-			} else if(command.execute(this)==false){
-				System.err.println("Error: incorrect execution of command");
+			while(!end){
+				String rawInput = scanner.nextLine();
+				Command command;
+				command = CommandParser.parse(rawInput);
+				if (command.equals(null)){
+					System.err.println("Error: incorrect command");
+				} else if(command.execute(this)==false){
+					System.err.println("Error: incorrect execution of command");
+				}
 			}
-			
 			
 			scanner.close();
 		}
+		 //METODOS PARA EJECUTAR COMANDOS
+		
 }
