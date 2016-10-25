729a0b1167634d1fe51c04ea8dd4c420fd0edc1e
Raúl Torrijos <raultorr@ucm.es>
2016-10-25 19:14:45 +0300

    secondaryRawInput builder added
    Will be used as an argument for some ByteCodeParser methods

--- src/CommandParser.java
+++ src/CommandParser.java
@@ -3,9 +3,14 @@ public class CommandParser{
 
 	
 		public static Command parse(String line){
-			Command command = null;										//Initializes to null
+			Command command = null;	
+			ByteCode intruction = null;	//Initializes to null
 			String[] arrayOfStrings = line.split(" ");					//Splits the big string into an array of string divided by white spaces
+			String secondaryRawInput=arrayOfStrings[1];
 			
+			for (int i=2; i<arrayOfStrings.length; i++) {
+				secondaryRawInput=secondaryRawInput+" "+arrayOfStrings[i];
+			} //POSIBLE ERROR WHEN PARSING THE INSTRUCTION DUE TO THE ADDITION OF SPACES PERFORMED
 			
 			if (arrayOfStrings[0].equalsIgnoreCase("help")) {	
 				command = new Command(Command.ENUM_COMMAND.help);
@@ -13,6 +18,7 @@ public class CommandParser{
 				command = new Command(Command.ENUM_COMMAND.quit);
 			} else if(arrayOfStrings[0].equalsIgnoreCase("newinst")) {
 				command = new Command(Command.ENUM_COMMAND.newinst);
+				instruction
 			} else if(arrayOfStrings[0].equalsIgnoreCase("run")) {
 				command = new Command(Command.ENUM_COMMAND.run);
 			} else if(arrayOfStrings[0].equalsIgnoreCase("reset")) {
